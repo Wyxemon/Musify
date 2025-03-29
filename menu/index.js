042b2fd3060b11318311873f8a5f2d446bec82aa
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-29 08:39:01 +0100

    Add music stop

--- menu/index.js
+++ menu/index.js
@@ -1,4 +1,16 @@
+const audio = document.getElementById('audio');
+const img = document.getElementById('play__img')
+var running = 1
 function playAudio() {
-    const audio = document.getElementById('audio');
-    audio.play()
+    if (running==1) {
+        audio.play()
+        img.src = "pause.png"
+        img.style = 'width: 15px; height: 15px;'
+        running = 0
+    } else {
+        audio.pause()
+        img.src = "play.png"
+        img.style = 'width: 25px; height: 20px;'
+        running = 1
+    }
 }
