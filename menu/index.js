a4521776123a2708949c14581f6b8f047ca4eb9c
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-30 14:57:32 +0200

    Add bottom menu

--- menu/index.js
+++ menu/index.js
@@ -1,13 +1,17 @@
 const audio = document.getElementById('audio');
 const img = document.getElementById('play__img')
+var audioTime = 0
 var running = 1
-function playAudio() {
+function playAudio(music) {
     if (running==1) {
+        audio.currentTime = audioTime
+        audio.src = music
         audio.play()
         img.src = "pause.png"
         img.style = 'width: 15px; height: 15px;'
         running = 0
     } else {
+        audioTime = audio.currentTime.toFixed(2)
         audio.pause()
         img.src = "play.png"
         img.style = 'width: 25px; height: 20px;'
