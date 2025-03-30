ea695c564e255f4ec4ee791e72bf963daa68297f
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-30 15:22:36 +0200

    Sort the proyect

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
+/* COMO EJECUTAR (Electron) 
+1) Para ejecutar electron debes tener instalado Node.js
+2) Instalarlo con 'npm install'
+3) Ejecutar con 'npm run dev'
+*/
+
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         width: 1200,
