222bd82bb3d5d7ba6f4526aca8604e3c7d2dfed7
Wyxemon <142150209+Wyxemon@users.noreply.github.com>
2025-03-31 08:58:29 +0200

    Fix problem with bottom audio

--- menu/index.js
+++ menu/index.js
@@ -1,20 +1,23 @@
-const audio = document.getElementById('audio');
-const img = document.getElementById('play__img')
-var audioTime = 0
-var running = 1
+const audio = parent.document.getElementById('audio'); // Access audio from the main index.html
+const img = document.getElementById('play__img');
+var audioTime = 0;
+var running = 1;
+
 function playAudio(music) {
-    if (running==1) {
-        audio.currentTime = audioTime
-        audio.src = music
-        audio.play()
-        img.src = "pause.png"
-        img.style = 'width: 15px; height: 15px;'
-        running = 0
+    if (running == 1) {
+        if (audio.src !== music) {
+            audio.src = music;
+        }
+        audio.currentTime = audioTime;
+        audio.play();
+        img.src = "pause.png";
+        img.style = 'width: 15px; height: 15px;';
+        running = 0;
     } else {
-        audioTime = audio.currentTime.toFixed(2)
-        audio.pause()
-        img.src = "play.png"
-        img.style = 'width: 25px; height: 20px;'
-        running = 1
+        audioTime = audio.currentTime.toFixed(2);
+        audio.pause();
+        img.src = "play.png";
+        img.style = 'width: 25px; height: 20px;';
+        running = 1;
     }
 }
